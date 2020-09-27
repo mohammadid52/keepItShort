92c23b40cffc21bcfbd833b80685761bd74e7b0f
Mohammad <mohammadidehgamwala5@gmail.com>
2020-09-27 16:12:14 +0530

    folder structure changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Layout/Navbar";
 
 function App() {
-  return <div className="App">{/* Code Here */}</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Layout/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>Navbar Here</div>;
+};
+
+export default Navbar;
